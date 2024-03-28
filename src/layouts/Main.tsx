900113d8a45e0fdeb389e545868b0cc132bbca06
MShakhboz <mr.mirzomurotov96@mail.ru>
2024-03-28 11:34:02 +0500

    created layout

---
+++ src/layouts/Main.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import { SideBar } from "../components";
+
+const Main = () => {
+    return (
+        <div className="flex h-screen">
+            <div className="fixed h-full">
+                <SideBar />
+            </div>
+            <div className="flex-1 ml-28 flex items-start justify-center border border-emerald-500">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default Main;
