37f844d1af5c02d0b9430ed2c493c56ab9c4b326
MShakhboz <mr.mirzomurotov96@mail.ru>
2024-03-29 11:52:07 +0500

    updated content component

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,8 +1,14 @@
+import clsx from "clsx";
 import { Props } from "./type";
 
-const Content = ({ children }: Props) => {
+const Content = ({ children, className }: Props) => {
     return (
-        <div className="w-full min-h-max p-7 rounded-2xl bg-cs-white-100">
+        <div
+            className={clsx(
+                "w-full min-h-max p-7 rounded-2xl bg-cs-white-100",
+                className
+            )}
+        >
             {children}
         </div>
     );
