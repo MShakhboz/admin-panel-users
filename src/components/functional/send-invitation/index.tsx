103e2494d3bbad8a6c8b6a7364d7ecd2dbce3a72
MShakhboz <mr.mirzomurotov96@mail.ru>
2024-03-30 02:36:57 +0500

    send invitaion modal updated

--- src/components/functional/send-invitation/index.tsx
+++ src/components/functional/send-invitation/index.tsx
@@ -1,14 +1,13 @@
 import { Controller, useForm } from "react-hook-form";
 import { Button, FlexBox, Input, Title } from "../..";
 import Dropdown from "../../drop-down";
-import Modal from "../../modal";
 import { CrossIcon } from "../../svgs";
 import { Props } from "./type";
 import { OPTIONS_PERMISSIONS } from "./constant";
 import { useAppDispatch } from "../../../store/hooks";
 import { addUser } from "../../../store/slices/usersSlice";
 
-const SendInvitation = ({ isOpen, onClose }: Props) => {
+const SendInvitation = ({ close }: Props) => {
     const dispatch = useAppDispatch();
     const { handleSubmit, control, reset } = useForm({});
 
@@ -16,16 +15,16 @@ const SendInvitation = ({ isOpen, onClose }: Props) => {
     const onSubmit = (data: any) => {
         dispatch(addUser(data));
         reset();
-        onClose();
+        close();
     };
 
     const handleClose = () => {
         reset();
-        onClose();
+        close();
     };
 
     return (
-        <Modal isOpen={isOpen} className={" pb-7"}>
+        <>
             <FlexBox className="justify-end">
                 <FlexBox
                     className="!w-[40px] h-[40px] justify-center bg-cs-gray-200 rounded-[15px] cursor-pointer"
@@ -34,7 +33,7 @@ const SendInvitation = ({ isOpen, onClose }: Props) => {
                     <CrossIcon />
                 </FlexBox>
             </FlexBox>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} className="mb-7">
                 <FlexBox className="flex-col justify-center">
                     <Title
                         title="Отправьте приглашение"
@@ -75,7 +74,7 @@ const SendInvitation = ({ isOpen, onClose }: Props) => {
                     />
                 </FlexBox>
             </form>
-        </Modal>
+        </>
     );
 };
 
--- src/components/functional/send-invitation/type.ts
+++ src/components/functional/send-invitation/type.ts
@@ -1,4 +1,4 @@
 export type Props = {
     isOpen: boolean;
-    onClose: () => void;
+    close: () => void;
 };
