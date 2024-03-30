27b007f5ab8b401c3c742cac557134d4f9e0a210
MShakhboz <mr.mirzomurotov96@mail.ru>
2024-03-30 06:35:14 +0500

    improved inputs

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -9,7 +9,7 @@ const Input = ({ id, name, value, onChange, type, placeholder }: Props) => {
             onChange={onChange}
             placeholder={placeholder}
             type={type}
-            className="border rounded-[15px] w-full max-w-[404px] p-[17.5px] border-cs-gray-350 text-cs-black-500 outline-none placeholder:text-cs-gray-400"
+            className="border rounded-[15px] w-full max-w-[404px] p-[17.5px] border-cs-gray-350 text-cs-black-500 bg-transparent outline-none placeholder:text-cs-gray-400"
         />
     );
 };
--- src/components/search-input/index.tsx
+++ src/components/search-input/index.tsx
@@ -9,7 +9,7 @@ const SearchInput = ({ placeholder, className, onChange }: Props) => {
                 type="text"
                 placeholder={placeholder}
                 onChange={onChange}
-                className="w-full px-3 py-[5px] border border-gray-300 text-cs-black-500 font-normal rounded-[10px] outline-none focus:border-cs-green-400 transition-all duration-300 ease-in-out"
+                className="w-full px-3 py-[5px] border bg-transparent border-gray-300 text-cs-black-500 font-normal rounded-[10px] outline-none focus:border-cs-green-400 transition-all duration-300 ease-in-out"
             />
             <SearchIcon className="absolute top-3 right-3" />
         </div>
